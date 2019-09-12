cee4cf6b2ed68482ba28bd2d284ce43b2974b3f7
recidvst <cdsnowden@gmail.com>
2019-09-12 17:47:46 +0100

    update users model

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,21 +1,31 @@
 // get mongoose to work with mongodb
 const mongoose = require('mongoose');
+const connections = require('../db.js');
 
 // define a schema
 const Schema = mongoose.Schema;
 let userSchema = new Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    email: {
-        type: String, 
-        required: [true, 'Please provide a valid email.'],
-    },
-    password: {
-        type: String, 
-        required: [true, 'Please provide a password.'],
-    }
+  _id: mongoose.Schema.Types.ObjectId,
+  email: {
+    type: String,
+    required: [true, 'Please provide a valid email.'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+  }
 }, {
-    timestamps: true
+  timestamps: true
 });
 
-// export the schema as a model for use in app
-module.exports = mongoose.model('User', userSchema, 'users');
+// moby dick db user model
+var MobyUserModel = connections.mobyMongooseConnection.model('User', userSchema, 'users');
+
+// alice in wonderland db user model
+var AliceUserModel = connections.aliceMongooseConnection.model('User', userSchema, 'users');
+
+// export the models for use in app
+module.exports = {
+  MobyUserModel: MobyUserModel,
+  AliceUserModel: AliceUserModel
+};
