e6a558a8f5676d7db320ee096c791dd24996555f
recidvst <cdsnowden@gmail.com>
2020-04-03 15:27:10 +0100

    add count and random arguments

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var {
   GraphQLString,
   GraphQLList,
   GraphQLFloat,
+  GraphQLInt,
+  GraphQLBoolean,
   GraphQLNonNull,
   GraphQLError,
 } = require('graphql');
@@ -56,27 +58,60 @@ const RootQueryType = new GraphQLObjectType({ // root query
       args: {
         book: { type: GraphQLString },
         _id: { type: GraphQLID },
+        count: { type: GraphQLInt },
+        random: { type: GraphQLBoolean },
       },
       resolve: (obj, args, context, info) => {
+        // CHECKS //
         // book must be avail if passed
-        if (args.book && args.book.length > 0 && ['moby', 'moby-dick', 'alice'].indexOf(args.book) < 0) {
+        if (args.book && args.book.length > 0 && ['moby', 'moby-dick', 'moby_dick', 'alice', 'alice-in-wonderland', 'alice_in_wonderland'].indexOf(args.book) < 0) {
           throw new GraphQLError ("Book name argument must be valid and available");
         };
         // id must be right format if passed
         if (args._id && !mongoose.Types.ObjectId.isValid(args._id)) {
           throw new GraphQLError ("ID argument name must be a valid Mongoose ObjectId");
         };
+        // count must be right format if passed (INT)
+        if (args.count && (!Number.isInteger(args.count) || args.count < 1)) {
+          throw new GraphQLError ("Count argument name must be a positive Integer");
+        };
+        // random must be right format if passed (BOOL)
+        if (args.random && typeof args.random !== "boolean") {
+          throw new GraphQLError ("Random argument name must be a Boolean");
+        };
+
+        // DETERMINE AMOUNT
+        let queryAmount = 1; // default
+        if (args.count) {
+          queryAmount = args.count;
+        }
+
+        // DETERMINE RANDOM
+        let random = false; // default
+        if (args.random && args.random === true) {
+          random = args.random;
+        }
+
         // return moby or alice
         if (args.book && args.book.toLowerCase().trim() === 'alice') {
           if (args._id) {
             return titlesMongoModels.AliceTitleModel.find( {_id: args._id} );
           }
-          return titlesMongoModels.AliceTitleModel.find( {} );
+          if (random) {
+            return titlesMongoModels.AliceTitleModel.aggregate( [ { $sample: { size : queryAmount} } ]);
+          } else {
+            return titlesMongoModels.AliceTitleModel.find( {} ).limit(queryAmount);
+          }
+
         } else {
           if (args._id) {
             return titlesMongoModels.MobyTitleModel.find( {_id: args._id} );
           }
-          return titlesMongoModels.MobyTitleModel.find( {} );
+          if (random) {
+            return titlesMongoModels.MobyTitleModel.aggregate( [ { $sample: { size : queryAmount} } ]);
+          } else {
+            return titlesMongoModels.MobyTitleModel.find( {} ).limit(queryAmount);
+          }
         }
       },
     },
@@ -86,27 +121,60 @@ const RootQueryType = new GraphQLObjectType({ // root query
       args: {
         book: { type: GraphQLString },
         _id: { type: GraphQLID },
+        count: { type: GraphQLInt },
+        random: { type: GraphQLBoolean },
       },
       resolve: (obj, args, context, info) => {
+        // CHECKS //
         // book must be avail if passed
-        if (args.book && args.book.length > 0 && ['moby', 'moby-dick', 'alice'].indexOf(args.book) < 0) {
+        if (args.book && args.book.length > 0 && ['moby', 'moby-dick', 'moby_dick', 'alice', 'alice-in-wonderland', 'alice_in_wonderland'].indexOf(args.book) < 0) {
           throw new GraphQLError ("Book name argument must be valid and available");
         };
         // id must be right format if passed
         if (args._id && !mongoose.Types.ObjectId.isValid(args._id)) {
           throw new GraphQLError ("ID argument name must be a valid Mongoose ObjectId");
         };
+        // count must be right format if passed (INT)
+        if (args.count && (!Number.isInteger(args.count) || args.count < 1)) {
+          throw new GraphQLError ("Count argument name must be a positive Integer");
+        };
+        // random must be right format if passed (BOOL)
+        if (args.random && typeof args.random !== "boolean") {
+          throw new GraphQLError ("Random argument name must be a Boolean");
+        };
+
+        // DETERMINE AMOUNT
+        let queryAmount = 1; // default
+        if (args.count && Number.isInteger(args.count)) {
+          queryAmount = args.count;
+        }
+
+        // DETERMINE RANDOM
+        let random = false; // default
+        if (args.random && args.random === true) {
+          random = args.random;
+        }
+
         // return moby or alice
         if (args.book && args.book.toLowerCase().trim() === 'alice') {
           if (args._id) {
             return paragraphsMongoModels.AliceParagraphModel.find( {_id: args._id} );
           }
-          return paragraphsMongoModels.AliceParagraphModel.find( {} );
+          if (random) {
+            return paragraphsMongoModels.AliceParagraphModel.aggregate( [ { $sample: { size : queryAmount} } ]);
+          } else {
+            return paragraphsMongoModels.AliceParagraphModel.find( {} ).limit(queryAmount);
+          }
+
         } else {
           if (args._id) {
             return paragraphsMongoModels.MobyParagraphModel.find( {_id: args._id} );
           }
-          return paragraphsMongoModels.MobyParagraphModel.find( {} );
+          if (random) {
+            return paragraphsMongoModels.MobyParagraphModel.aggregate( [ { $sample: { size : queryAmount} } ]);
+          } else {
+            return paragraphsMongoModels.MobyParagraphModel.find( {} ).limit(queryAmount);
+          }
         }
       },
     }
