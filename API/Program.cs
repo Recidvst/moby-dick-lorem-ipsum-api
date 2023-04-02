d5824ae67eec70db6fa987aa2bba69748751d575
Chris Snowden <cdsnowden@gmail.com>
2023-04-02 17:17:47 +0100

    chore: add dotnet-format

--- API/Program.cs
+++ API/Program.cs
@@ -10,8 +10,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-app.UseSwagger();
-app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
