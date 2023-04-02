27a543767b5998974e07c946c43aef02a71cd78c
Chris Snowden <cdsnowden@gmail.com>
2023-04-02 16:50:43 +0100

    feat: bootstrap dotnet solution to replace node api

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router({mergeParams: true});
-
-router.get('/', (request, response, next) => {
-  response.status(200).send("Welcome to the Moby Dick Lorem Ipsum generator API. \nCheck the docs @ https://github.com/Recidvst/moby-dick-lorem-ipsum for endpoint info.");
-});
-
-module.exports = router;
